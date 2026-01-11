a25d3df0bf040547e3cba10fc0290b595649e57c
Joseph Koshakow <koshy44@gmail.com>
2026-01-11 12:57:23 -0500

    Send clients command responses (#9)
    Previously, the gRPC server would immediately send a success response
    back to the client after receiving the request, as long as the server
    was still running running. This commit updates the gRPC server so that
    it waits for the command to commit before sending a success response. If
    the command cannot commits, or times out, then an error response is sent
    to the client.

--- server/src/main.rs
+++ server/src/main.rs
@@ -5,6 +5,7 @@ use andross_service::kv::raft_service_server::RaftServiceServer;
 use clap::Parser;
 use raft::storage::MemStorage;
 use std::collections::HashMap;
+use std::time::Duration;
 use tonic::transport::Server;
 
 #[derive(Parser)]
@@ -21,6 +22,9 @@ struct Args {
     /// Map of node IDs to hostnames and ports (e.g., 1=localhost:42667).
     #[arg(long, value_parser = parse_peer)]
     peers: Vec<(u64, String)>,
+
+    #[arg(long, default_value = "100ms", value_parser = humantime::parse_duration)]
+    default_request_timeout: Duration,
 }
 
 fn parse_peer(s: &str) -> std::result::Result<(u64, String), String> {
@@ -43,7 +47,8 @@ async fn main() -> Result<()> {
 
     let peers: HashMap<u64, String> = args.peers.into_iter().collect();
 
-    let (node, node_handle) = initialize::<MemStorage>(args.id, peers).await?;
+    let (node, node_handle) =
+        initialize::<MemStorage>(args.id, peers, args.default_request_timeout).await?;
 
     // TODO: Add way to shutdown the node.
     let _node_task_handle = tokio::spawn(async move {
