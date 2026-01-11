c064190882080e468a1313066a2d7006a38b76d0
Joseph Koshakow <koshy44@gmail.com>
2026-01-11 00:09:04 -0500

    Use Bytes for byte arrays (#8)
    This commit updates most usages of Vec<u8> with Bytes to avoid cloning
    large byte arrays.

--- service/build.rs
+++ service/build.rs
@@ -1,4 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_prost_build::configure().compile_protos(&["proto/kv/v1/kv.proto"], &["proto"])?;
+    tonic_prost_build::configure()
+        .bytes(".")
+        .compile_protos(&["proto/kv/v1/kv.proto"], &["proto"])?;
     Ok(())
 }
