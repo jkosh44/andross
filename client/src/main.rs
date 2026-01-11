3dd315ccc010da535fa44c55e20292a8f6846339
Joseph Koshakow <koshy44@gmail.com>
2026-01-10 22:51:01 -0500

    Support multiple nodes (#5)
    This commit adds support for more than one server node to be running at
    a time.

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,30 +1,34 @@
 use andross_service::kv::CommandRequest;
-use andross_service::kv::raft_service_client::RaftServiceClient;
+use andross_service::kv::kv_service_client::KvServiceClient;
 use clap::Parser;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// The port to listen on.
-    #[arg(short, long, default_value_t = 42666)]
-    port: u16,
+    /// Addresses of the servers to connect to, in the format `host:port`.
+    #[arg(short, long, default_values = ["[::1]:42666"])]
+    addrs: Vec<String>,
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() {
     let args = Args::parse();
 
-    let port = args.port;
-    let mut client = RaftServiceClient::connect(format!("http://[::1]:{port}")).await?;
+    for addr in args.addrs {
+        match send_request(addr).await {
+            Ok(()) => return,
+            Err(e) => println!("CLIENT ERROR: {e}"),
+        }
+    }
+}
 
+async fn send_request(addr: String) -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = KvServiceClient::connect(format!("http://{addr}")).await?;
     // TODO: Support user defined requests.
     let request = tonic::Request::new(CommandRequest {
         data: b"42 666".to_vec(),
     });
-
     let response = client.command(request).await?;
-
     println!("RESPONSE={response:?}");
-
     Ok(())
 }
