ce5a7a030cf8db7aa066516dbcccb179f2a4deec
Joseph Koshakow <koshy44@gmail.com>
2026-01-10 23:56:29 -0500

    Support arbitrary client requests (#7)

--- client/src/main.rs
+++ client/src/main.rs
@@ -5,29 +5,31 @@ use clap::Parser;
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Args {
+    /// Command to write to the server.
+    #[arg(long)]
+    command: String,
+
     /// Addresses of the servers to connect to, in the format `host:port`.
-    #[arg(short, long, default_values = ["[::1]:42666"])]
+    #[arg(long, default_values = ["[::1]:42666"])]
     addrs: Vec<String>,
 }
 
 #[tokio::main]
 async fn main() {
     let args = Args::parse();
+    let data = args.command.into_bytes();
 
     for addr in args.addrs {
-        match send_request(addr).await {
+        match send_request(addr, data.clone()).await {
             Ok(()) => return,
             Err(e) => println!("CLIENT ERROR: {e}"),
         }
     }
 }
 
-async fn send_request(addr: String) -> Result<(), Box<dyn std::error::Error>> {
+async fn send_request(addr: String, data: Vec<u8>) -> Result<(), Box<dyn std::error::Error>> {
     let mut client = KvServiceClient::connect(format!("http://{addr}")).await?;
-    // TODO: Support user defined requests.
-    let request = tonic::Request::new(CommandRequest {
-        data: b"42 666".to_vec(),
-    });
+    let request = tonic::Request::new(CommandRequest { data });
     let response = client.command(request).await?;
     println!("RESPONSE={response:?}");
     Ok(())
