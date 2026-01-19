2808bc89baad504ac54b76af4a937fea8b538765
Joseph Koshakow <koshy44@gmail.com>
2026-01-19 15:22:21 -0500

    Apply committed log entries (#16)
    This commit applies committed log entries by writing them into an
    embedded LSM tree.

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,39 +1,57 @@
-use andross_server::parse_uri;
-use andross_server::service::CommandRequest;
+use andross_server::service::InsertRequest;
 use andross_server::service::kv_service_client::KvServiceClient;
+use andross_server::{Tuple, parse_uri};
 use bytes::Bytes;
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// Command to write to the server.
-    #[arg(long)]
-    command: String,
-
     /// Addresses of the servers to connect to, in the format `host:port`.
     #[arg(long, default_values = ["[::1]:42666"])]
     addrs: Vec<String>,
+
+    /// Command to send to the server.
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Inserts a new key-value pair into the database.
+    Insert {
+        #[arg(long)]
+        key: Bytes,
+        #[arg(long)]
+        value: Bytes,
+    },
 }
 
 #[tokio::main]
 async fn main() {
     let args = Args::parse();
-    let data = Bytes::from(args.command.into_bytes());
-
-    for addr in args.addrs {
-        match send_request(addr, data.clone()).await {
-            Ok(()) => return,
-            Err(e) => println!("CLIENT ERROR: {e}"),
+    match args.command {
+        Commands::Insert { key, value } => {
+            for addr in args.addrs {
+                match send_request(addr, key.clone(), value.clone()).await {
+                    Ok(()) => return,
+                    Err(e) => println!("CLIENT ERROR: {e}"),
+                }
+            }
         }
     }
 }
 
-async fn send_request(addr: String, data: Bytes) -> Result<(), Box<dyn std::error::Error>> {
+async fn send_request(
+    addr: String,
+    key: Bytes,
+    value: Bytes,
+) -> Result<(), Box<dyn std::error::Error>> {
     let uri = parse_uri(&addr)?;
     let mut client = KvServiceClient::connect(uri).await?;
-    let request = tonic::Request::new(CommandRequest { data });
-    let response = client.command(request).await?;
+    let tuple = Tuple::from_key_value(&key, &value).into_bytes();
+    let request = tonic::Request::new(InsertRequest { tuple });
+    let response = client.insert(request).await?;
     println!("RESPONSE={response:?}");
     Ok(())
 }
